7f9423f3b46c72465da23b405395691d1f0e5f54
kojimadaiki <swim1205@ezweb.ne.jp>
2020-12-24 14:22:37 +0900

    userバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :debates
+  has_many :comments
+
+  with_options presence: true do
+    validates :name
+    validates :style
+  end 
 end
