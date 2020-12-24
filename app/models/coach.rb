bd963c6a74e7582720785f808a119ba35e3e4d35
kojimadaiki <swim1205@ezweb.ne.jp>
2020-12-24 14:22:47 +0900

    coachバリデーションの設定

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -3,4 +3,13 @@ class Coach < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :debates
+  has_many :comments
+  belongs_to :bulletin_board
+
+  with_options presence: true do
+    validates :name
+    validates :teach_style
+  end 
 end
