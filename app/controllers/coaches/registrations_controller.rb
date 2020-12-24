9405cf481e20e4e37d74e16b8a38e6107fbc9d27
kojimadaiki <swim1205@ezweb.ne.jp>
2020-12-24 12:32:06 +0900

    コントローラーのアクションの設定

--- app/controllers/coaches/registrations_controller.rb
+++ app/controllers/coaches/registrations_controller.rb
@@ -5,29 +5,35 @@ class Coaches::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+  end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # DELETE /resource
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+  end
+
+  protected
+  
+  def after_sign_up_path_for(resource)
+    new_coach_registration_path
+  end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
