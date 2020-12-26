1de3935090527ff234636cf3d6896dfda129404e
kojimadaiki <swim1205@ezweb.ne.jp>
2020-12-26 11:48:40 +0900

    debate_userテーブルにカラム追加

--- db/migrate/20201226023634_create_debate_users.rb
+++ db/migrate/20201226023634_create_debate_users.rb
@@ -1,7 +1,8 @@
 class CreateDebateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :debate_users do |t|
-
+      t.references :user, foreign_key: true
+      t.references :debate, foreign_key: true
       t.timestamps
     end
   end
