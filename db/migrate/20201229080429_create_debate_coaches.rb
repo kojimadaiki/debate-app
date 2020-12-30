c6da84fdffdec3a750131a563084081e097be856
kojimadaiki <swim1205@ezweb.ne.jp>
2020-12-30 20:41:43 +0900

    debate_coachのテーブル作成

---
+++ db/migrate/20201229080429_create_debate_coaches.rb
@@ -0,0 +1,9 @@
+class CreateDebateCoaches < ActiveRecord::Migration[6.0]
+  def change
+    create_table :debate_coaches do |t|
+      t.references :debate, foreign_key: true
+      t.references :coach, foreign_key: true
+      t.timestamps
+    end
+  end
+end
