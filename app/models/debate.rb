19a616fc7d4537923600237850a38133796ac46a
kojimadaiki <swim1205@ezweb.ne.jp>
2020-12-30 20:38:06 +0900

    debateモデルのアソシエーション設定

--- app/models/debate.rb
+++ app/models/debate.rb
@@ -1,6 +1,8 @@
 class Debate < ApplicationRecord
-  has_many :users, through: :debate_users
-  has_many :coaches, through: :debate_coaches
-  has_many :debate_users
-  has_many :debate_coaches
+  has_many :debate_users, dependent: :destroy
+  has_many :users, through: :debate_users, validate: false
+  has_many :debate_coaches, dependent: :destroy
+  has_many :coaches, through: :debate_coaches, validate: false
+
+  validates :name, presence: true
 end
