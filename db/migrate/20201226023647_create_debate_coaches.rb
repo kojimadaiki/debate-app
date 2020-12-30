a106f40cd7ff2ec3792c5594d7c72132625947b8
kojimadaiki <swim1205@ezweb.ne.jp>
2020-12-30 20:40:20 +0900

    ビューとマイグレーションファイルの削除

--- db/migrate/20201226023647_create_debate_coaches.rb
+++
@@ -1,9 +0,0 @@
-class CreateDebateCoaches < ActiveRecord::Migration[6.0]
-  def change
-    create_table :debate_coaches do |t|
-      t.references :coach, foreign_key: true
-      t.references :debate, foreign_key: true
-      t.timestamps
-    end
-  end
-end
