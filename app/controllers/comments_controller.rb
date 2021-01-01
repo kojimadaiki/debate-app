42aba279914726a26261cde618039caa89060893
kojimadaiki <swim1205@ezweb.ne.jp>
2021-01-01 16:08:03 +0900

    commentコントローラー編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,20 +5,34 @@ class CommentsController < ApplicationController
     # @comments = @debate.comments.includes(:coach)
     # @comments = @debate.comments.includes(:user)
   end
-  
+
   def create
     @debate = Debate.find(params[:debate_id])
-    @comment = @debate.messages.new(comment_params)
-    if @comment.save
+    if coach_signed_in?
+      @comment = @debate.comments.new(coach_comment_params)
+    else user_signed_id?
+      @comment = @debate.comments.new(user_comment_params)
+    end
+    if (@comment || @comments).save 
       redirect_to debate_comments_path(@debate)
     else
+      # @comments = @debate.comments.includes(:coach)
+      # @comments = @debate.comments.includes(:user)
       render :index
     end
   end
-  
+
   private
-  
-  def comment_params
-    params.require(:comment).permit(:content).merge(user_id: current_user.id, coach_id: current_coach.id)
+
+  def coach_comment_params
+    params.require(:comment).permit(:content).merge(coach_id: current_coach.id)
+    if current_coach.id.present?
+    end
+  end
+
+  def user_comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id)
+    if current_user.id.present?
+    end
   end
 end
