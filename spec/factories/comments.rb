351c479e565665b4c69c23188fb2ae136dcbf923
kojimadaiki <swim1205@ezweb.ne.jp>
2021-01-05 16:41:05 +0900

    commentのテストコードの作成

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,4 +1,13 @@
 FactoryBot.define do
   factory :comment do
+    content {Faker::Lorem.sentence}
+    association :user
+    association :coach
+    association :debate
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+      message.video.attach(io: File.open('public/videos/dressel.MP4'), filename: 'dressel.MP4')
+    end
   end
 end
