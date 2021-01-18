beb598e351cb55a2d1ada7801a7374d3f7ecfbea
kojimadaiki <swim1205@ezweb.ne.jp>
2021-01-18 16:58:35 +0900

    Revert "ユーザー管理コントローラーの記述"
    This reverts commit dea437ef7bebeeacac5c97adf08178d71c4cf147.

--- app/controllers/coaches/sessions_controller.rb
+++ app/controllers/coaches/sessions_controller.rb
@@ -2,8 +2,6 @@
 
 class Coaches::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  skip_before_action :verify_authenticity_token
-
 
   # GET /resource/sign_in
   def new
