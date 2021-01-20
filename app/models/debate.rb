52a4dcb90d3a0579732fa72e3ac0e6834cb1936d
kojimadaiki <swim1205@ezweb.ne.jp>
2021-01-20 14:25:30 +0900

    ビュー編集

--- app/models/debate.rb
+++ app/models/debate.rb
@@ -1,6 +1,6 @@
 class Debate < ApplicationRecord
   has_many :debate_coaches, dependent: :destroy
-  has_many :coaches, through: :debate_coaches, validate: false
+  has_many :coaches, through: :debate_coaches, validate: false, dependent: :destroy
   has_many :comments, dependent: :destroy
 
   validates :name, presence: true
